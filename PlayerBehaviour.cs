a68aad358a9366a9481700a4f211e594464df5c2
Jérémy Legendre <50450932+JeremyLegendre@users.noreply.github.com>
2022-01-07 20:42:32 +0100

    pull mode

--- PlayerBehaviour.cs
+++ PlayerBehaviour.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerBehaviour : MonoBehaviour
+public class Grapple : MonoBehaviour
 {
     public Camera mainCamera;
     public Material lineMaterial;
     private LineRenderer lineRenderer;
     private Vector2 target;
     private Vector3 targetPosition;
+    private GameObject targetObject;
     public float speed = 15.0f;
+    public float rangeLimit = 10.0f;
 
     private bool hasDirectionTooReach = false;
+    private bool hasObjectToPull = false;
+    public string mode = "reach"; // reach or pull
 
     public void Start()
     {
@@ -24,57 +28,69 @@ public class PlayerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (hasDirectionTooReach)
+        if (mode == "reach" && hasDirectionTooReach)
         {
-            RemoveGravityImpactOnPlayer();
             PursueMovementToTarget();
-        } else
+        } else if (mode == "pull" && hasObjectToPull)
         {
-            ResetGravityImpactOnPlayer();
+            PursuePullingObject();
         }
-        
+
         if (Input.GetMouseButtonDown(0))
         {
-            hasDirectionTooReach = false;
-            StartReachTarget();
+            if (mode == "reach")
+            {
+                StartReachTarget();
+            } else if (mode == "pull")
+            {
+                StartPullObject();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) == true)
+        {
+            if (hasDirectionTooReach == false && hasObjectToPull == false)
+            {
+                mode = mode == "reach" ? "pull" : "reach";
+            }
         }
     }
 
     private void StartReachTarget()
     {
+        hasDirectionTooReach = false;
         Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
 
-        //If something was hit, the RaycastHit2D.collider will not be null.
-        if (hit.collider != null && hit.collider.tag == "Grappable")
+        // If something was hit, the RaycastHit2D.collider will not be null.
+        if (hit.collider != null && hit.collider.tag == "Reachable")
         {
             hasDirectionTooReach = true;
             targetPosition = Input.mousePosition;
             target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-            // add force impulse
-            addImpulseForceToReachTarget();
-            
-
-            // launch attraction to target
+            // launch movement to target
             PursueMovementToTarget();
-        } else
+        }
+        else
         {
             lineRenderer.enabled = false;
         }
     }
 
-    private void addImpulseForceToReachTarget()
+    private void StartPullObject()
     {
-        // calculate needed force to reach position
-        float xDistance = transform.position.x - targetPosition.x;
-        float yDistance = Mathf.Abs(transform.position.y - targetPosition.y);
+        Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
 
-        Debug.Log(0.001f * xDistance);
-        Debug.Log(0.05f * yDistance);
-
-        Vector2 force = new Vector2(0, 0.05f * yDistance);
-        GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
+        //If something was hit, the RaycastHit2D.collider will not be null.
+        if (hit.collider != null && hit.collider.tag == "Pullable")
+        {
+            hasObjectToPull = true;
+            target = Camera.main.ScreenToWorldPoint(hit.collider.gameObject.transform.position);
+            targetPosition = Input.mousePosition;
+            targetObject = hit.collider.gameObject;
+        }
     }
 
     private void PursueMovementToTarget()
@@ -101,14 +117,45 @@ public class PlayerBehaviour : MonoBehaviour
         }
     }
 
+    private void PursuePullingObject()
+    {
+        if (target != Vector2.zero)
+        {
+            float distanceBewteenTarget = Vector2.Distance(targetObject.transform.position, transform.position);
+
+            if (distanceBewteenTarget > 1)
+            {
+                targetObject.transform.position = Vector2.MoveTowards(targetObject.transform.position, transform.position, speed * Time.deltaTime);
+                Vector2 currentPosition = transform.position;
+
+                lineRenderer.SetPosition(0, targetObject.transform.position);
+                lineRenderer.SetPosition(1, currentPosition);
+
+                lineRenderer.positionCount = 2;
+                lineRenderer.enabled = true;
+            }
+            else
+            {
+                lineRenderer.enabled = false;
+                hasObjectToPull = false;
+            }
+        }
+        else
+        {
+            lineRenderer.enabled = false;
+            hasObjectToPull = false;
+        }
+    }
+
     private void DrawLine()
     {
         Vector2 targetPosition;
 
-        if(hasDirectionTooReach == true)
+        if (hasDirectionTooReach == true || hasObjectToPull == true)
         {
             targetPosition = target;
-        } else
+        }
+        else
         {
             targetPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
         }
@@ -119,18 +166,5 @@ public class PlayerBehaviour : MonoBehaviour
 
         lineRenderer.positionCount = 2;
         lineRenderer.enabled = true;
-    }
-
-    private void RemoveGravityImpactOnPlayer()
-    {
-        /*if (rigidbody.bodyType == RigidbodyType2D.Dynamic)
-        {
-            rigidbody.bodyType = RigidbodyType2D.Static;
-        }*/
-    }
-
-    private void ResetGravityImpactOnPlayer() 
-    {
-        //rigidbody.bodyType = RigidbodyType2D.Dynamic;
     }
 }
